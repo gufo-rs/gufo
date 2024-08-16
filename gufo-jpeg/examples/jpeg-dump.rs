26a05759f80ca2950719ff8ae6aa95aae1d980e2
Sophie Herold <sophie@hemio.de>
2024-08-16 21:21:37 +0200

    jpeg: Add example that lists segments in file

---
+++ gufo-jpeg/examples/jpeg-dump.rs
@@ -0,0 +1,23 @@
+use gufo_jpeg::Jpeg;
+
+fn main() {
+    let path = std::env::args()
+        .nth(1)
+        .expect("First agument must be a file path.");
+    let data = std::fs::read(path).unwrap();
+    let jpeg = Jpeg::new(&data);
+
+    for segment in jpeg.segments() {
+        let data_init = segment
+            .data()
+            .iter()
+            .take(50)
+            .take_while(|x| **x != b'\0')
+            .filter(|x| x.is_ascii_graphic())
+            .cloned()
+            .collect::<Vec<_>>();
+        let s = String::from_utf8_lossy(&data_init);
+
+        println!("{:x?}: {s}", segment.marker());
+    }
+}
