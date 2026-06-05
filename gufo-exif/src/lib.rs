28c09ce6934ab3959491529d4f0fd72bbd72b505
Sophie Herold <sophie@hemio.de>
2026-06-05 20:18:43 +0200

    gufo-exif: Don't fix lifetime to static

--- gufo-exif/src/lib.rs
+++ gufo-exif/src/lib.rs
@@ -6,3 +6,5 @@ pub use error::*;
 pub use exif::{ExifInternal, Storage};
 
 pub type Exif = ExifInternal<'static, exif::OwnedStore>;
+pub type ExifMutBorrowed<'a> = ExifInternal<'a, exif::MutBorrowedStore<'a>>;
+pub use structure::Typed;
